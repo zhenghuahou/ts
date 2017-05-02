64b7f046fecdd8825e82e2f8616f576e77086414
侯正华 <houzhenghua.sh@superjia.com>
2017-05-02 14:37:55 +0800

    add

---
+++ demo1/index.ts
@@ -0,0 +1,15 @@
+interface Shape {
+    name: string;
+    width: number;
+    height: number;
+    color?: string;
+}
+ 
+function area(shape : Shape) {
+    var area = shape.width * shape.height;
+    return "I'm " + shape.name + " with area " + area + " cm squared";
+}
+ 
+console.log( area( {name: "rectangle", width: 30, height: 15} ) );
+console.log( area( {name: "square", width: 30, height: 30, color: "blue"} ) );
+//console.log( area( {width: 30, height: 15} ) );
